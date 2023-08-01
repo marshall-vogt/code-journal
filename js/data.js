644701cf8f2ae1ffe1c5df6ff2ffe4ae0713026c
Marshall Vogt <vogt_marshall@yahoo.com>
2023-08-01 05:50:26 +0000

    Added 'beforeunload' event listener to window object

--- js/data.js
+++ js/data.js
@@ -6,3 +6,10 @@ let data = {
   editing: null,
   nextEntryId: 1,
 };
+
+window.addEventListener('beforeunload', handleUnload);
+
+function handleUnload(event) {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJSON);
+}
