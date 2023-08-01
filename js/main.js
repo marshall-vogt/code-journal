431d84ce6e095c2a183d0f9fe22743ca5ca83c29
Marshall Vogt <vogt_marshall@yahoo.com>
2023-08-01 17:56:21 +0000

    Defined renderEntry function and added content load event listener to document to generate DOM tree for all entries

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Task #5- Add Photo URL event listener
+// Issue 1, Task #5- Add Photo URL event listener
 
 const $photoUrl = document.querySelector('#photo-url');
 const $image = document.querySelector('img');
@@ -9,7 +9,7 @@ function updatePhoto(event) {
   $image.setAttribute('src', $photoUrl.value);
 }
 
-// Task #6- Add submit event listener
+// Issue 1, Task #6- Add submit event listener
 
 const $entryForm = document.querySelector('form');
 
@@ -27,4 +27,46 @@ function handleSubmit(event) {
   data.entries.unshift(inputValues);
   $image.setAttribute('src', 'images/placeholder-image-square.jpg');
   $entryForm.reset();
+}
+
+// Issue 2, Task #4
+
+function renderEntry(entry) {
+  const $li = document.createElement('li');
+  const $div1 = document.createElement('div');
+  const $div2 = document.createElement('div');
+  const $div3 = document.createElement('div');
+  const $img = document.createElement('img');
+  const $h2 = document.createElement('h2');
+  const $p = document.createElement('p');
+  $div1.setAttribute('class', 'row');
+  $div2.setAttribute('class', 'column-half');
+  $div3.setAttribute('class', 'column-half');
+  $img.setAttribute('src', data.entries[entry].photoURL);
+  $h2.setAttribute('class', 'entry-title');
+  $p.setAttribute('class', 'entry-content');
+  $h2.textContent = data.entries[entry].title;
+  $p.textContent = data.entries[entry].notes;
+
+  $li.appendChild($div1);
+  $div1.appendChild($div2);
+  $div1.appendChild($div3);
+  $div2.appendChild($img);
+  $div3.appendChild($h2);
+  $div3.appendChild($p);
+
+  return $li;
+}
+
+// Issue 2, Task #5
+
+const $ul = document.querySelector('ul');
+
+document.addEventListener('DOMContentLoaded', generateDomTree);
+
+function generateDomTree(event) {
+  for (let i = 0; i < data.entries.length; i++) {
+    const entryDom = renderEntry(i);
+    $ul.appendChild(entryDom);
+  }
 }
