30b7c928a70f3fa2d17e660110611e37d3f53cf7
Marshall Vogt <vogt_marshall@yahoo.com>
2023-08-01 05:53:38 +0000

    Added conditional statement to parse previous data entry

--- js/data.js
+++ js/data.js
@@ -13,3 +13,8 @@ function handleUnload(event) {
   const dataJSON = JSON.stringify(data);
   localStorage.setItem('javascript-local-storage', dataJSON);
 }
+
+const previousDataJSON = localStorage.getItem('javascript-local-storage');
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
